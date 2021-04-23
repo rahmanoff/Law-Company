6149d0b5b7d3c51b0dc0726b4d2cf732dfd6822d
rahmanoff <rahmanoff@gmail.com>
2021-04-23 17:25:17 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function() {
+    $('.slider__inner').slick({
+        prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+        infinite: false,
+    });
+    $('select').styler();
+});
