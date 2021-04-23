ea3ac13e364fe9a77dea4e3a649853510e926f38
rahmanoff <rahmanoff@gmail.com>
2021-04-23 23:00:20 +0300

    News added

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 $(function() {
-    $('.slider__inner').slick({
+    $('.slider__inner, .news__slider-inner').slick({
         prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
         nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
         infinite: false,
     });
     $('select').styler();
+
 });
